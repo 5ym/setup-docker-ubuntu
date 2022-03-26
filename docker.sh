a56ad7c22ae950e39ba5c28bdff276f106615cfb
Yui Maruyama <16yuim@gmail.com>
2022-03-26 15:44:38 +0900

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt-get -y remove docker docker-engine docker.io containerd runc
-sudo apt-get -y update
+sudo apt-get update
 sudo apt-get -y install \
     ca-certificates \
     curl \
@@ -11,7 +11,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get -y update
+sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
