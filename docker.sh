8678002e397658d7fbbad3367e84e1f42821bbd0
Yui Maruyama <16yuim@gmail.com>
2022-03-29 12:47:28 +0900

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -13,7 +13,7 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/2.3.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl enbale docker
 sudo adduser $USER docker
